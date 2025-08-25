199dc939bacf7b46a087246121626579e3c63151
kudy <bayardkudy1@gmail.com>
2025-08-26 00:45:19 +0100

    Get author

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,26 @@
+﻿using LibraryManagement.Models;
+using LibraryManagement.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagement.Controllers;
+
+[ApiController]
+[Route("api/[Controller]")]
+public class AuthorsController : ControllerBase
+{
+    private readonly LibraryDbContext _context;
+
+    public AuthorsController(LibraryDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Author>> GetAuthor([FromRoute] int id)
+    {
+        var author = await _context.Authors.FindAsync(id);
+
+        return author is null ?
+            NotFound() : Ok(author);
+    }
+}
