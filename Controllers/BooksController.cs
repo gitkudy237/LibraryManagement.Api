d77fe8aad42acd4ca37dae93dc1af688620a5dd4
kudy <bayardkudy1@gmail.com>
2025-08-26 02:24:33 +0100

    Add book controller

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,36 @@
+﻿using LibraryManagement.Models;
+using LibraryManagement.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly LibraryDbContext _context;
+
+        public BooksController(LibraryDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Book>> GetBook([FromRoute] int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+
+            return book is null ?
+                NotFound() : Ok(book);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> GetBooks()
+        {
+            var books = await _context.Books.ToListAsync();
+            return Ok(books);
+        }
+    }
+}
