ecbf51a33de283e8a97206ecdcb1d07764834de6
kudy <bayardkudy1@gmail.com>
2025-08-26 02:08:18 +0100

    Seed books

--- Persistence/ApplicationDbSeeder.cs
+++ Persistence/ApplicationDbSeeder.cs
@@ -1,4 +1,5 @@
 ﻿using LibraryManagement.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibraryManagement.Persistence;
 
@@ -8,15 +9,52 @@ public static class ApplicationDbSeeder
     {
         if (!context.Authors.Any())
         {
-            var data = new Author[]
+            var authors = new Author[]
             {
                 new Author{Name = "Author1", Bio = "Author1-Bio", DateOfBirth = new DateOnly(1995, 06, 16)},
                 new Author{Name = "Author2", Bio = "Author2-Bio", DateOfBirth = new DateOnly(1996, 07, 26)},
                 new Author{Name = "Author3", Bio = "Author3-Bio", DateOfBirth = new DateOnly(1990, 12, 6)},
             };
 
-            await context.AddRangeAsync(data);
+            await context.Authors.AddRangeAsync(authors);
             await context.SaveChangesAsync();
+
+        }
+
+        if (!context.Books.Any())
+        {
+            var books = new Book[]
+            {
+                new Book
+                {
+                    Title = "Title1", 
+                    Isbn = Guid.NewGuid().ToString(), 
+                    PublicationYear = 2017, 
+                    AvailableCopies = 5, 
+                    AuthorId =  context.Authors.FirstOrDefault(a => a.Name == "Author1")!.Id
+                },
+                new Book
+                {
+                    Title = "Title2", 
+                    Isbn = Guid.NewGuid().ToString(), 
+                    PublicationYear = 2019, 
+                    AvailableCopies = 8, 
+                    AuthorId =  context.Authors.FirstOrDefault(a => a.Name == "Author1")!.Id
+                },
+                new Book
+                {
+                    Title = "Title3", 
+                    Isbn = Guid.NewGuid().ToString(), 
+                    PublicationYear = 2017, 
+                    AvailableCopies = 7, 
+                    AuthorId =  context.Authors.FirstOrDefault(a => a.Name == "Author2")!.Id
+                },
+
+            };
+
+            await context.Books.AddRangeAsync(books);
+            await context.SaveChangesAsync();
+
         }
     }
 }
