43cf4f8fb1c023388ec2e18a55106a51d5da1edd
kudy <bayardkudy1@gmail.com>
2025-08-28 23:17:42 +0100

    Get borrowings from repository

--- Controllers/BorrowingsController.cs
+++ Controllers/BorrowingsController.cs
@@ -1,3 +1,4 @@
+using LibraryManagement.Core.Abstractions;
 using LibraryManagement.Dtos.BorrowingsDto;
 using LibraryManagement.Mappings;
 using LibraryManagement.Persistence;
@@ -10,17 +11,20 @@ namespace LibraryManagement.Controllers
     [Route("api/[controller]")]
     public class BorrowingsController : ControllerBase
     {
-        private readonly LibraryDbContext _context;
+        private readonly IBorrowingRepository _borrowingRepository;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public BorrowingsController(LibraryDbContext context)
+        public BorrowingsController(IBorrowingRepository borrowingRepository, IUnitOfWork unitOfWork)
         {
-            _context = context;
+            _borrowingRepository = borrowingRepository;
+            _unitOfWork = unitOfWork;
         }
+
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<BorrowingDto>>> GetAllBorrowings()
         {
-            var borrowings = await _context.Borrowings.ToListAsync();
+            var borrowings = await _borrowingRepository.GetAllAsync();
             var result = borrowings.Select(b => b.ToBorrowingDto());
 
             return Ok(result);
@@ -29,10 +33,10 @@ namespace LibraryManagement.Controllers
         [HttpGet("{id:int}")]
         public async Task<ActionResult<BorrowingDto>> GetBorrowing(int id)
         {
-            var borrowing = await _context.Borrowings.FindAsync(id);
+            var borrowing = await _borrowingRepository.GetByIdAsync(id, includeRelated: true);
 
             return borrowing is null ?
-                NotFound() : Ok(borrowing.ToBorrowingDto());
+                NotFound() : Ok(borrowing);
         }
     }
 }
--- Core/Abstractions/IBorrowingRepository.cs
+++ Core/Abstractions/IBorrowingRepository.cs
@@ -4,7 +4,7 @@ namespace LibraryManagement.Core.Abstractions
 {
     public interface IBorrowingRepository
     {
-        Task<Borrowing?> GetByIdAsync(int id, bool includeRelated);
+        Task<Borrowing?> GetByIdAsync(int id, bool includeRelated = false);
         Task<IEnumerable<Borrowing>> GetAllAsync();
     }
 }
